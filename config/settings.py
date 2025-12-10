27add0e9aab5642fcfe18c813778f382eda077e4
javadzamanifard <javadzamanii.1374@gmail.com>
2025-12-10 05:50:16 -0800

    feat: Implement postgresql database and its settings

--- config/settings.py
+++ config/settings.py
@@ -14,18 +14,18 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+from environs import Env
+env = Env()
+env.read_env()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h^a+ptm%q%x@^s=6!#(37o*1za@kyx_0gev%q$ud3t_1d@d#1q'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+SECRET_KEY = env.str('DJANGO_SECRET_KEY')
+DEBUG = env.bool('DJANGO_DEBUG')
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS')
 
 
 # Application definition
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.str('DATABASE_NAME'),          
+        'USER': env.str('DATABASE_USER'),       
+        'PASSWORD': env.str('DATABASE_PASWWORD'), 
+        'HOST': 'localhost',            
+        'PORT': '5432', 
     }
 }
 
